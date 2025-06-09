e09e31a159ec56bd54e6b792c6e672735bdd178a
Callmesammy <callmesammy@yahoo.com>
2025-06-09 22:26:56 +0100

    update

--- app/(root)/_components/cool-body.tsx
+++ app/(root)/_components/cool-body.tsx
@@ -23,6 +23,13 @@ const Coolbody = () => {
           <span className=" absolute w-62 bg-white flex h-15 items-center text-lg px-2 z-10 rounded-full border-2 border-black top-3 text-black"> Instead of selling a book...</span>
             <Image src={"/sadbk.svg"} fill alt="img" className="object-contain flex w-72 items-center justify-center "/>
 </div>   
+<div className="flex  w-full h-[400px]  bg-yellow-500 md:px-8 relative">
+          <span className=" absolute w-62 bg-white flex h-15 items-center text-lg px-2 z-10 rounded-full border-2 border-black top-3 text-black"> ...start by selling a blog post!</span>
+            <Image src={"/blog.svg"} fill alt="img" className="object-contain flex w-72 items-center justify-center "/>
+
+            <Image src={"/excite.svg"} height={3} width={2} alt="img" className=" absolute bottom-0 flex w-32 right-0 items-center justify-center "/>
+
+</div>   
 
   </div>
     );
