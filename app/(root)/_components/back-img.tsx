acdac77347efb58a0e3f03bc351ec022d4292661
Callmesammy <callmesammy@yahoo.com>
2025-06-09 17:29:32 +0100

    up

--- app/(root)/_components/back-img.tsx
+++ app/(root)/_components/back-img.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const BckImg = () => {
     return ( 
-        <div className="flex w-full h-full relative not-first:">
+        <div className="flex w-full h-full relative overflow-hidden">
             <Image src="/001.png" alt="image" width={200} height={150} className="absolute top-40 left-30 transition-transform opacity-35 animate-bounce duration-1000 ease-in-out " />
             <Image src="/003.png" alt="image" width={200} height={150} className="absolute top-90 left-0  transition-transform opacity-75 scale-100 animate-pulse duration-1000 ease-in-out" />
             <Image src="/004.png" alt="image" width={150} height={150} className="absolute top-10 right-30 transition-transform opacity-75 scale-100 animate-pulse duration-1000 ease-in-out"/>
