1050d9faf42f88ea9fe864f6d4f3860556af3238
Callmesammy <callmesammy@yahoo.com>
2025-06-09 22:29:25 +0100

    update

--- app/(root)/_components/cool-body.tsx
+++ app/(root)/_components/cool-body.tsx
@@ -10,21 +10,21 @@ const mulish = Mulish({
 const Coolbody = () => {
     return (  
         <div className="grid md:grid-cols-2 w-full">
-            <div className="flex  w-full h-62 items-center bg-pink-900/85 md:px-8 ">
+            <div className="flex border border-black w-full h-72 items-center bg-pink-900/85 md:px-8 ">
             <h1 className={`${mulish.className} text-center flex text-4xl px-2 pt-3`}>Don't take risks. That's scary!</h1>
 
 </div>
-<div className="flex  w-full h-62 items-center bg-pink-400 md:px-8 ">
+<div className="flex  w-full h-72 items-center border border-black bg-pink-400 md:px-8 ">
             <h1 className={`${mulish.className} text-center flex text-4xl px-2 pt-3`}>Place small bets. That's exciting!</h1>
 
 </div>      
 
-<div className="flex  w-full h-[400px]  bg-black md:px-8 relative">
+<div className="flex  w-full h-[400px]   bg-black md:px-8 relative">
           <span className=" absolute w-62 bg-white flex h-15 items-center text-lg px-2 z-10 rounded-full border-2 border-black top-3 text-black"> Instead of selling a book...</span>
             <Image src={"/sadbk.svg"} fill alt="img" className="object-contain flex w-72 items-center justify-center "/>
 </div>   
 <div className="flex  w-full h-[400px]  bg-yellow-500 md:px-8 relative">
-          <span className=" absolute w-62 bg-white flex h-15 items-center text-lg px-2 z-10 rounded-full border-2 border-black top-3 text-black"> ...start by selling a blog post!</span>
+          <span className=" absolute w-72 bg-white flex h-15 items-center text-lg px-2 z-10 rounded-full border-2 border-black top-3 text-black"> ...start by selling a blog post!</span>
             <Image src={"/blog.svg"} fill alt="img" className="object-contain flex w-72 items-center justify-center "/>
 
             <Image src={"/excite.svg"} height={3} width={2} alt="img" className=" absolute bottom-0 flex w-32 right-0 items-center justify-center "/>
