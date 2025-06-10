d6c55e279d4587c8b99203c7a38c28c57ab24f6c
Callmesammy <callmesammy@yahoo.com>
2025-06-10 11:25:40 +0100

    update

--- app/(root)/_components/down-writeups.tsx
+++ app/(root)/_components/down-writeups.tsx
@@ -14,7 +14,7 @@ const Wrightups = () => {
             <div className="w-full flex-col flex h-82 items-center justify-center">
             <h2 className={` ${mulish.className} tracking-wide bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-5xl md:text-5xl lg:text-6xl font-sans py-2 md:py-10 relative z-20 font-bold `}>
             Make your own road </h2>
-            <p className="text-xl items-center flex w-132 text-center">
+            <p className="text-xl items-center flex w-full truncate text-center">
             Whether you need more balance, flexibility, or just a different gig, we make it easier to chart a new path. You don't have to be a tech expert or even understand how to start a business. You just gotta take what you know and sell it.
 
             </p>
