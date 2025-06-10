bd1be9a4b372e8bbf08cb84afb091e2e3c6302bc
Callmesammy <callmesammy@yahoo.com>
2025-06-10 13:47:06 +0100

    save

--- app/(root)/_components/remarks.tsx
+++ app/(root)/_components/remarks.tsx
@@ -1,6 +1,6 @@
 const Remarks = () => {
     return (
-        <div className="h-screen  pt-4 grid grid-cols-2 bg-pink-300">
+        <div className="h-screen  pt-4 grid grid-cols-2 bg-pink-400">
             Document
             <div>
                 s
