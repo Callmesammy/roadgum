411ab98558e0a0190a07fb9da111a1c08515b6e0
Callmesammy <callmesammy@yahoo.com>
2025-06-19 15:37:44 +0100

    update

--- app/(root)/_components/lastpage.tsx
+++ app/(root)/_components/lastpage.tsx
@@ -1,7 +1,14 @@
 const Lastpage = () => {
     return ( 
-        <div>
-            LastPage
+        <div className="flex h-[400px] px-3 pt-4 bg-black text-white">
+            <div className="grid lg:grid-cols-2 w-full px-4">
+                <div className="w-full h-full flex flex-col">
+                <p className="text-3xl font-semibold">  Subscribe to get tips and tactics to grow the way you want.</p>
+
+
+                </div>
+
+            </div>
         </div>
      );
 }
--- app/(root)/_components/remarks3.tsx
+++ app/(root)/_components/remarks3.tsx
@@ -17,7 +17,7 @@ const Remarks3 = () => {
             </div>
             <div className="w-full h-[700px] relative flex items-center ">
             <Image src={"/steph.png"} alt="max image" fill  className="object-contain lg:pt-3 flex z-10 "/>
-            <Image src={"/steph.svg"} alt="max image" width={120} height={100} className="z-10 left-4 object-contain pt-[100px] flex lg:absolute"/>
+            <Image src={"/steph.svg"} alt="max image" width={150} height={100} className="z-10 md:left-42 object-contain pt-[100px] flex md:absolute"/>
             <Image src={"/stepdolls.svg"} alt="max image" width={120} height={100} className="z-10 right-[150px] object-contain pt-[100px]  lg:absolute hidden lg:flex"/>
 
             <div className="absolute  group group-hover:translate-x-2 group-hover:translate-y-2 bottom-[90px] transition-transform flex items-center justify-center w-42 h-13 bg-black md:right-42 insert-0  lg:right-32 right-12 z-20 p-2 rounded-full">
@@ -57,22 +57,22 @@ The amount of income earned by Gumroad digital entrepreneurs last week.</div>
 </div>
 
 
-<div className="w-full px-3 h-[300px] space-y-5 flex-col relative items-center justify-center text-center bg-pink-400 flex ">
+<div className="w-full px-3 h-[300px] space-y-5 flex-col relative items-center justify-center text-center bg-pink-400/70 flex ">
 
 <h1 className="text-5xl font-semibold tracking-wider"> Share your work. Someone out there needs it.  </h1>
 
-<Link href={"/"}  className="flex md:w-62 group relative w-full pt-6">
+<Link href={"/"}  className="flex lg:w-62 group relative w-full pt-6">
       
-      <div className="w-full justify-center z-20 flex items-center p-4  text-muted-foreground   bg-yellow-400 transition-transform rounded-xl border border-muted-foreground">                 
+      <div className="w-full justify-center z-20 flex items-center p-4  text-muted-foreground   bg-yellow-400 transition-transform rounded-sm border border-muted-foreground">                 
       <h1 className="text-xl "> Start selling</h1>
 
 
  </div>
-      <span className=" w-full justify-center text-center cursor-pointer z-30 p-4 flex items-center absolute  bg-black   text-white group-hover:-translate-y-2 group-hover:-translate-x-1 transition-transform rounded-xl border border-muted-foreground">
+      <span className=" w-full justify-center text-center cursor-pointer z-30 p-4 flex items-center absolute  bg-black   text-white group-hover:-translate-y-2 group-hover:-translate-x-1 transition-transform rounded-sm border border-muted-foreground">
      <h1 className="text-xl  "> Start selling</h1>
 
          </span> 
-         <span className="w-full justify-center z-10 cursor-pointer p-4 flex items-center absolute  bg-red-500   text-white group-hover:translate-y-2 group-hover:translate-x-1 transition-transform rounded-xl border border-muted-foreground">
+         <span className="w-full justify-center z-10 cursor-pointer p-4 flex items-center absolute  bg-red-500   text-white group-hover:translate-y-2 group-hover:translate-x-1 transition-transform rounded-sm border border-muted-foreground">
      <h1 className="text-xl "> Start selling</h1>
 
          </span>
